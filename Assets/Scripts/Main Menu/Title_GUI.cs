12e40fb5708e35eb8b023d217cfb44bdcc930903
Otazuno <otazuno@gmail.com>
2014-08-16 13:09:13 -0400

    Settings Window Updated
    -Finished Graphics Window Design
    -Almost finished Sound Window Design

--- Assets/Scripts/Main Menu/Title_GUI.cs
+++ Assets/Scripts/Main Menu/Title_GUI.cs
@@ -5,6 +5,20 @@ using System.Xml;
 using System;
 
 public class Title_GUI : Hex_GUI {
+	GUIContent[] resolutionList;
+	private ComboBox resolutionControl = new ComboBox();
+	GUIContent[] visualQualityList;
+	private ComboBox visualQualityControl = new ComboBox();
+	GUIContent[] interfaceSizeList;
+	private ComboBox interfaceSizeControl = new ComboBox();
+	GUIContent[] antiAliasingList;
+	private ComboBox antiAliasingControl = new ComboBox();
+	GUIContent[] textureQualityList;
+	private ComboBox textureQualityControl = new ComboBox();
+	GUIContent[] shadowQualityList;
+	private ComboBox shadowQualityControl = new ComboBox();
+	GUIContent[] shaderQualityList;
+	private ComboBox shaderQualityControl = new ComboBox();
 
 	// GUI Styles
 	private static GUIStyle MainMenuButton;
@@ -20,10 +34,13 @@ public class Title_GUI : Hex_GUI {
 	private static GUIStyle LoadButton;
 	private static GUIStyle MainMenuToggle;
 	private static GUIStyle SaveLabel;
-	private static GUIStyle GeneralButton;
-	private static GUIStyle GraphicsButton;
-	private static GUIStyle SoundButton;
-	private static GUIStyle ControlsButton;
+	private static GUIStyle GeneralToggle;
+	private static GUIStyle GraphicsToggle;
+	private static GUIStyle SoundToggle;
+	private static GUIStyle ControlsToggle;
+	private static GUIStyle SettingsWindow;
+	private static GUIStyle DropDownButton;
+	private static GUIStyle DropDownMenu;
 
 	// Load-Window Textures
 	private static Texture2D autosaveTexture = (Texture2D)Resources.Load ("Textures/autosave");
@@ -41,13 +58,15 @@ public class Title_GUI : Hex_GUI {
 	static Rect mainMenuWindow = new Rect(40, 30, 238, 184);
 	static Rect versionWindow = new Rect(742, 529, 165, 20); 
 	static Rect loadGameWindow = new Rect(619, 30, 341, 542);
-	static Rect settingsTabWindow = new Rect(619, 30, 50, 200);
+	static Rect settingsTabWindow = new Rect(619, 30, 50, 542);
 	static Rect settingsWindow = new Rect(669, 30, 291, 542);
 	static Rect exitWindow = new Rect((Screen.width/2)-(0.24792f*Screen.width/2), (Screen.height/2)-(0.17833f*Screen.height/2), 0.24792f*Screen.width, 0.17833f*Screen.height);
 
 	// Window Booleans
 	private Dictionary<string, bool> mainMenuBool = new Dictionary<string, bool>();
 	private string mainMenuSelection = "NONE";
+	private Dictionary<string, bool> settingsBool = new Dictionary<string, bool>();
+	private string settingsSelection = "GENERAL";
 
 	// String Variables
 	string loadGame = "none";
@@ -62,11 +81,63 @@ public class Title_GUI : Hex_GUI {
 	public float masterVolume = 5.0F;
 	public float backgroundVolume = 5.0F;
 	public float effectsVolume = 5.0F;
+	public float dialogVolume = 5.0F;
+	public float uiVolume = 5.0F;
+	public float qualityVolume = 5.0F;
 	public float mouseSensitivity = 5.0F;
 
 	private void Awake(){
 		LoadMainMenu();
+		LoadSettingsTabs();
 		guiAlpha = 1.0f;
+
+		resolutionList = new GUIContent[8];
+		resolutionList[0] = new GUIContent("Window");
+		resolutionList[1] = new GUIContent("800x600");
+		resolutionList[2] = new GUIContent("1024x600");
+		resolutionList[3] = new GUIContent("1024x768");
+		resolutionList[4] = new GUIContent("1280x720");
+		resolutionList[5] = new GUIContent("1280x768");
+		resolutionList[6] = new GUIContent("1360x768");
+		resolutionList[7] = new GUIContent("Fullscreen");
+		resolutionControl.SetSelectedItemIndex(7);
+
+		visualQualityList = new GUIContent[3];
+		visualQualityList[0] = new GUIContent("Low");
+		visualQualityList[1] = new GUIContent("Medium");
+		visualQualityList[2] = new GUIContent("High");
+		visualQualityControl.SetSelectedItemIndex(1);
+		
+		interfaceSizeList = new GUIContent[3];
+		interfaceSizeList[0] = new GUIContent("Small");
+		interfaceSizeList[1] = new GUIContent("Normal");
+		interfaceSizeList[2] = new GUIContent("Large");
+		interfaceSizeControl.SetSelectedItemIndex(1);
+
+		antiAliasingList = new GUIContent[3];
+		antiAliasingList[0] = new GUIContent("None");
+		antiAliasingList[1] = new GUIContent("2X");
+		antiAliasingList[2] = new GUIContent("4X");
+		antiAliasingControl.SetSelectedItemIndex(1);
+
+		textureQualityList = new GUIContent[3];
+		textureQualityList[0] = new GUIContent("Low");
+		textureQualityList[1] = new GUIContent("Medium");
+		textureQualityList[2] = new GUIContent("High");
+		textureQualityControl.SetSelectedItemIndex(1);
+
+		shadowQualityList = new GUIContent[4];
+		shadowQualityList[0] = new GUIContent("Off");
+		shadowQualityList[1] = new GUIContent("Low");
+		shadowQualityList[2] = new GUIContent("Medium");
+		shadowQualityList[3] = new GUIContent("High");
+		shadowQualityControl.SetSelectedItemIndex(2);
+
+		shaderQualityList = new GUIContent[3];
+		shaderQualityList[0] = new GUIContent("Low");
+		shaderQualityList[1] = new GUIContent("Medium");
+		shaderQualityList[2] = new GUIContent("High");
+		shaderQualityControl.SetSelectedItemIndex(1);
 	}
 
 	// Load Main Menu
@@ -74,6 +145,14 @@ public class Title_GUI : Hex_GUI {
 		mainMenuBool.Add("LOAD GAME", false);
 		mainMenuBool.Add("SETTINGS", false);
 		mainMenuBool.Add("EXIT", false);
+	}
+	
+	// Load Settings
+	private void LoadSettingsTabs(){
+		settingsBool.Add("GENERAL", true);
+		settingsBool.Add("GRAPHICS", false);
+		settingsBool.Add("SOUND", false);
+		settingsBool.Add("CONTROLS", false);
 	}
 	
 	// Set Main Menu
@@ -90,6 +169,19 @@ public class Title_GUI : Hex_GUI {
 			else
 				mainMenuBool[key] = false;
 			this.mainMenuSelection = mainMenuSelection;
+		}
+	}
+	
+	// Set Settings
+	private void SetSettingsTabs(string settingsSelection){
+		List<string> keys = new List<string>(settingsBool.Keys);
+		
+		foreach(string key in keys){
+			if (key == settingsSelection)
+				settingsBool[key] = true;
+			else
+				settingsBool[key] = false;
+			this.settingsSelection = settingsSelection;
 		}
 	}
 
@@ -113,10 +205,13 @@ public class Title_GUI : Hex_GUI {
 		LoadButton = skin.GetStyle ("LoadButton");
 		MainMenuToggle = skin.GetStyle("MainMenuToggle");
 		SaveLabel = skin.GetStyle("SaveLabel");
-		GeneralButton = skin.GetStyle("GeneralButton");
-		GraphicsButton = skin.GetStyle("GraphicsButton");
-		SoundButton = skin.GetStyle("SoundButton");
-		ControlsButton = skin.GetStyle("ControlsButton");
+		GeneralToggle = skin.GetStyle("GeneralToggle");
+		GraphicsToggle = skin.GetStyle("GraphicsToggle");
+		SoundToggle = skin.GetStyle("SoundToggle");
+		ControlsToggle = skin.GetStyle("ControlsToggle");
+		SettingsWindow = skin.GetStyle("SettingsWindow");
+		DropDownButton = skin.GetStyle("DropDownButton");
+		DropDownMenu = skin.GetStyle("DropDownMenu");
 	}
 
 	// Load GUI
@@ -152,8 +247,8 @@ public class Title_GUI : Hex_GUI {
 
 	// Load Settings Window
 	private void LoadSettingsWindow(){
-		settingsTabWindow = GUI.Window (4, settingsTabWindow, CreateTabsWindow, "", SettingsTabWindow);
-		settingsWindow = GUI.Window (5, settingsWindow, CreateSettingsWindow, "");	
+		settingsTabWindow = GUI.Window (4, settingsTabWindow, CreateSettingsTabsWindow, "", SettingsTabWindow);
+		settingsWindow = GUI.Window (5, settingsWindow, CreateSettingsWindow, "", SettingsWindow);	
 	}
 
 	// Load Exit Window
@@ -266,31 +361,40 @@ public class Title_GUI : Hex_GUI {
 	}
 
 	// Create Tabs Window
-	void CreateTabsWindow(int windowID){
-		if (GUI.Button(new Rect(0, 0, 50, 50), "", GeneralButton))
-			settingsTab = "general";
-		if (GUI.Button (new Rect (0, 50, 50, 50), "", GraphicsButton))
-			settingsTab = "graphics";
-		if (GUI.Button(new Rect(0, 100, 50, 50), "", SoundButton))
-			settingsTab = "sound";
-		if (GUI.Button(new Rect(0, 150, 50, 50), "", ControlsButton))
-			settingsTab = "controls";
+	void CreateSettingsTabsWindow(int windowID){
+		CreateSettingsTabsToggles ();
+	}
+	
+	// Create Main Menu Toggles
+	private void CreateSettingsTabsToggles(){
+		CreateSettingsTabsToggle(new Rect(0, 0, 50, 50), ref GeneralToggle, "GENERAL");
+		CreateSettingsTabsToggle(new Rect(0, 50, 50, 50), ref GraphicsToggle, "GRAPHICS");
+		CreateSettingsTabsToggle(new Rect(0, 100, 50, 50), ref SoundToggle, "SOUND");
+		CreateSettingsTabsToggle(new Rect(0, 150, 50, 50), ref ControlsToggle, "CONTROLS");
+	}
+	
+	// Create Settings Toggle
+	private void CreateSettingsTabsToggle(Rect newRect, ref GUIStyle settingsToggle, string settingsSelection){
+		if (GUI.Toggle(newRect, settingsBool[settingsSelection], "", settingsToggle) != settingsBool[settingsSelection])
+			SetSettingsTabs(settingsSelection);
 	}
 
 	// Create Settings Window
 	void CreateSettingsWindow(int windowID){
-		if (settingsTab == "general")
+		if (settingsBool["GENERAL"])
 			GeneralItems ();
-		else if (settingsTab == "graphics")
+		else if (settingsBool["GRAPHICS"])
 			GraphicsItems();
-		else if (settingsTab == "sound")
+		else if (settingsBool["SOUND"])
 			SoundItems();
-		else if (settingsTab == "controls")
+		else if (settingsBool["CONTROLS"])
 			ControlsItems();
-		if (GUI.Button(new Rect(10, 493, 131, 39), "APPLY")){
+		GUI.Button(new Rect(10, 431, 131, 39), "DEFAULT", LoadButton);
+		GUI.Button(new Rect(150, 431, 131, 39), "RESET", LoadButton);
+		if (GUI.Button(new Rect(10, 482, 131, 39), "APPLY", LoadButton)){ //493
 			mainMenuBool["SETTINGS"] = false;
 		}
-		if (GUI.Button(new Rect(150, 493, 131, 39), "CANCEL")){
+		if (GUI.Button(new Rect(150, 482, 131, 39), "CANCEL", LoadButton)){ //493
 			settingsTab = "general";
 			mainMenuBool["SETTINGS"] = false;
 		}
@@ -298,35 +402,62 @@ public class Title_GUI : Hex_GUI {
 
 	// General Settings
 	void GeneralItems(){
-		GUI.Label(new Rect(0.01042f*Screen.width, 0.01667f*Screen.height, 0.12708f*Screen.width, 0.04667f*Screen.height), "GENERAL", HeaderLabel);
+		GUI.Label(new Rect(10, 10, 153, 25), "GENERAL", HeaderLabel);
 	}
 
 	// Graphics Settings
 	void GraphicsItems(){
-		GUI.Label(new Rect(0.01042f*Screen.width, 0.01667f*Screen.height, 0.12708f*Screen.width, 0.04667f*Screen.height), "GRAPHICS", HeaderLabel);
-		GUI.Label(new Rect(0.02813f*Screen.width, 0.08833f*Screen.height, 0.08125f*Screen.width, 0.035f*Screen.height), "Display");
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.14833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Resolution");
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.20833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Quality");
+		GUI.Label(new Rect(10, 10, 153, 25), "GRAPHICS", HeaderLabel);
+		GUI.Label(new Rect(26, 49, 120, 20), "DISPLAY");
+		GUI.Label(new Rect(42, 78, 120, 20), "Resolution");
+		GUI.Label(new Rect(42, 118, 120, 20), "Visual Quality");
+		GUI.Label(new Rect(42, 158, 120, 20), "Interface Size");
+		GUI.Label(new Rect(42, 198, 120, 20), "Anti-Aliasing");
+		GUI.Label(new Rect(26, 257, 120, 20), "ADVANCED");
+		GUI.Label(new Rect(42, 286, 120, 20), "Texture Quality");
+		GUI.Label(new Rect(42, 326, 120, 20), "Shadow Quality");
+		GUI.Label(new Rect(42, 366, 120, 20), "Shader Quality");
+
+		int shaderQualityIndex = shaderQualityControl.GetSelectedItemIndex();
+		shaderQualityIndex = shaderQualityControl.List(new Rect(58, 386, 200, 20), shaderQualityList[shaderQualityIndex].text, shaderQualityList, DropDownButton, DropDownMenu, DropDownMenu);
+		int shadowQualityIndex = shadowQualityControl.GetSelectedItemIndex();
+		shadowQualityIndex = shadowQualityControl.List(new Rect(58, 346, 200, 20), shadowQualityList[shadowQualityIndex].text, shadowQualityList, DropDownButton, DropDownMenu, DropDownMenu);
+		int textureQualityIndex = textureQualityControl.GetSelectedItemIndex();
+		textureQualityIndex = textureQualityControl.List(new Rect(58, 306, 200, 20), textureQualityList[textureQualityIndex].text, textureQualityList, DropDownButton, DropDownMenu, DropDownMenu);
+		int antiAliasingIndex = antiAliasingControl.GetSelectedItemIndex();
+		antiAliasingIndex = antiAliasingControl.List(new Rect(58, 218, 200, 20), antiAliasingList[antiAliasingIndex].text, antiAliasingList, DropDownButton, DropDownMenu, DropDownMenu);
+		int interfaceSizeIndex = interfaceSizeControl.GetSelectedItemIndex();
+		interfaceSizeIndex = interfaceSizeControl.List(new Rect(58, 178, 200, 20), interfaceSizeList[interfaceSizeIndex].text, interfaceSizeList, DropDownButton, DropDownMenu, DropDownMenu);
+		int visualQualityIndex = visualQualityControl.GetSelectedItemIndex();
+		visualQualityIndex = visualQualityControl.List(new Rect(58, 138, 200, 20), visualQualityList[visualQualityIndex].text, visualQualityList, DropDownButton, DropDownMenu, DropDownMenu);
+		int resolutionIndex = resolutionControl.GetSelectedItemIndex();
+		resolutionIndex = resolutionControl.List(new Rect(58, 98, 200, 20), resolutionList[resolutionIndex].text, resolutionList, DropDownButton, DropDownMenu, DropDownMenu);
 	}
 
 	// Sound Settings
 	void SoundItems(){
-		GUI.Label(new Rect(0.01042f*Screen.width, 0.01667f*Screen.height, 0.12708f*Screen.width, 0.04667f*Screen.height), "SOUND", HeaderLabel);
-		GUI.Label(new Rect(0.02813f*Screen.width, 0.08833f*Screen.height, 0.08125f*Screen.width, 0.035f*Screen.height), "Volume");
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.14833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Master");
-		masterVolume = GUI.HorizontalSlider(new Rect(0.11667f*Screen.width, 0.14833f*Screen.height, 0.17188f*Screen.width, 0.035f*Screen.height), masterVolume, 0.0F, 10.0F);
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.20833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Background");
-		backgroundVolume = GUI.HorizontalSlider(new Rect(0.11667f*Screen.width, 0.20833f*Screen.height, 0.17188f*Screen.width, 0.035f*Screen.height), backgroundVolume, 0.0F, 10.0F);
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.26833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Effects");
-		effectsVolume = GUI.HorizontalSlider(new Rect(0.11667f*Screen.width, 0.26833f*Screen.height, 0.17188f*Screen.width, 0.035f*Screen.height), effectsVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(10, 10, 153, 25), "SOUND", HeaderLabel);
+		GUI.Label(new Rect(26, 49, 120, 20), "QUALITY");
+		qualityVolume = GUI.HorizontalSlider(new Rect(58, 78, 200, 20), qualityVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(26, 117, 120, 20), "VOLUME");
+		GUI.Label(new Rect(42, 146, 120, 20), "Master Volume");
+		masterVolume = GUI.HorizontalSlider(new Rect(58, 166, 200, 20), masterVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(42, 186, 120, 20), "Background");
+		backgroundVolume = GUI.HorizontalSlider(new Rect(58, 206, 200, 20), backgroundVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(42, 226, 120, 20), "Effects");
+		effectsVolume = GUI.HorizontalSlider(new Rect(58, 246, 200, 20), effectsVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(42, 266, 120, 20), "Dialog");
+		dialogVolume = GUI.HorizontalSlider(new Rect(58, 286, 200, 20), dialogVolume, 0.0F, 10.0F);
+		GUI.Label(new Rect(42, 306, 120, 20), "UI");
+		uiVolume = GUI.HorizontalSlider(new Rect(58, 326, 200, 20), uiVolume, 0.0F, 10.0F);
 	}
 
 	// Control Settings
 	void ControlsItems(){
-		GUI.Label(new Rect(0.01042f*Screen.width, 0.01667f*Screen.height, 0.12708f*Screen.width, 0.04667f*Screen.height), "CONTROLS", HeaderLabel);
-		GUI.Label(new Rect(0.02813f*Screen.width, 0.08833f*Screen.height, 0.08125f*Screen.width, 0.035f*Screen.height), "Mouse");
-		GUI.Label(new Rect(0.04583f*Screen.width, 0.14833f*Screen.height, 0.06354f*Screen.width, 0.035f*Screen.height), "Sensitivity");
-		mouseSensitivity = GUI.HorizontalSlider(new Rect(0.11667f*Screen.width, 0.14833f*Screen.height, 0.17188f*Screen.width, 0.035f*Screen.height), mouseSensitivity, 0.0F, 10.0F);
+		GUI.Label(new Rect(10, 10, 153, 25), "CONTROLS", HeaderLabel);
+		GUI.Label(new Rect(26, 49, 120, 20), "MOUSE");
+		GUI.Label(new Rect(42, 78, 120, 20), "Sensitivity");
+		mouseSensitivity = GUI.HorizontalSlider(new Rect(58, 98, 200, 20), mouseSensitivity, 0.0F, 10.0F);
 	}
 
 	// Exit
